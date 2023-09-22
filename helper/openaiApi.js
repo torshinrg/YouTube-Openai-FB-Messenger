ebda49accaf9d48ced1a98e9d8e1051b7d6da3c5
torshinrg <37323158+torshinrg@users.noreply.github.com>
2023-09-22 11:18:02 -0300

    Update openaiApi.js

--- helper/openaiApi.js
+++ helper/openaiApi.js
@@ -1,21 +1,24 @@
 require('dotenv').config();
-const { Configuration, OpenAIApi } = require("openai");
 
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+
+const OpenAI = require("openai")
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
 });
-const openai = new OpenAIApi(configuration);
+
 
 const chatCompletion = async (prompt) => {
 
     try {
-        const response = await openai.createChatCompletion(
+        const response = await openai.chat.completions.create(
             {
-                model: 'gpt-3.5-turbo',
+                model: "gpt-3.5-turbo",
                 messages: [
                     { "role": "system", "content": "You are a helpful assistant." },
                     { "role": "user", "content": prompt }
-                ]
+                ],
+                max_tokens: 30,
             }
         );
 
