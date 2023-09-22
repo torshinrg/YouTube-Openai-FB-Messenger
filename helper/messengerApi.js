2ad61b5dbadf72ce419e7e99a9270f87a38989d1
torshinrg <37323158+torshinrg@users.noreply.github.com>
2023-09-22 11:17:44 -0300

    Update messengerApi.js

--- helper/messengerApi.js
+++ helper/messengerApi.js
@@ -4,28 +4,48 @@ require('dotenv').config();
 const TOKEN = process.env.TOKEN;
 const PAGE_ID = process.env.PAGE_ID;
 
-const sendMessage = async(senderId, message) => {
+const sendMessage = async (senderId, message) => {
 
-    let options = {
-      method: 'POST',
-      url: `https://graph.facebook.com/v11.0/${PAGE_ID}/messages`,
-      params: {
-        access_token: TOKEN,
-        recipient: JSON.stringify({'id': senderId}),
-        messaging_type: 'RESPONSE',
-        message: JSON.stringify({'text': message})
+  let options = {
+    method: 'POST',
+    url: `https://graph.facebook.com/v11.0/${PAGE_ID}/messages`,
+    params: {
+      access_token: TOKEN,
+    },
+    data: {
+      recipient: {
+        id: senderId
+      },
+      messaging_type: 'RESPONSE',
+      message: {
+        text: message
       }
-    };
-    
-    let response = await axios.request(options);
-
-    if (response['status'] == 200 && response['statusText'] === 'OK') {
-        return 1;
-    } else {
-        return 0;
     }
+  };
+
+  
+
+  let response;
+  console.log("1 = " + options.url);
+  console.log("2 = " + options.params);
+
+  axios.post(options)
+    .then((resp) => {
+      let response = resp.data;
+      console.log("3 = " + response);
+    })
+    .catch((error) => {
+      console.log("4 = " + error);
+      console.log(error.message);
+    });
+
+  if (response != null && response['status'] == 200 && response['statusText'] === 'OK') {
+    return 1;
+  } else {
+    return 0;
+  }
 };
 
 module.exports = {
-    sendMessage
+  sendMessage
 }
